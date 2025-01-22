51d3830a564d98ab8c50b4d0f674a1456ae5ebb3
Nirmal1901 <74503593+Nirmal1901@users.noreply.github.com>
2025-01-22 11:51:53 +0530

    Convert Perl file to Python

---
+++ Folder/Folder1.py
@@ -0,0 +1,8 @@
+This is a simple Perl program that prints the string "Hello World" to the screen. Here's how you can convert it to Python:
+```python
+#!/usr/bin/env python
+
+# Print function
+print("Hello World")
+```
+Note that in Python, we use the `print()` function instead of `print` as in Perl. Also, in Python, the `#! /usr/bin/perl` shebang line is not necessary, since Python can be run directly by calling its interpreter.
