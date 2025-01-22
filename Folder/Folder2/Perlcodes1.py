bbe4b2a1d57bb1ea455fa48da0fcc2ccd217c6bd
Nirmal1901 <74503593+Nirmal1901@users.noreply.github.com>
2025-01-22 11:51:57 +0530

    Convert Perl file to Python

---
+++ Folder/Folder2/Perlcodes1.py
@@ -0,0 +1,12 @@
+Here's the equivalent Python code:
+```
+#!/usr/bin/env python
+
+# Import modules
+import warnings
+import sys
+
+# Print function
+print("Hello World")
+```
+Note that in Python, we use the `print()` function to output text to the console. The `#!` line is a shebang line that specifies the interpreter used to run the script (in this case, Python). The `import` statements import the necessary modules, and the `warnings` module is not necessary in this case, so it's commented out.
